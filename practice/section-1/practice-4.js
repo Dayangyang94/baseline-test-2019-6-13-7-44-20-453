194d994dda4192f68462ef3b8427b7e7144773ba
gaoyang <2436512082@qq.com>
2019-08-19 18:39:34 +0800

    实现

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var c=new Array();
+  var collB=objectB.value;
+  for(var i=0;i<collectionA.length;i++){
+    for(var j=0;j<collB.length;j++){
+      if(collectionA[i].key==collB[j]){
+        c.push(collectionA[i].key);
+        break;
+      }
+    }
+  } 
+  return c;
 }
